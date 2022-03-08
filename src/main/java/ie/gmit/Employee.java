a9ad23766a1ba6243e0cd2bf7b93c9cc4b6bc135
g00366561 <asharzafri45@gmail.com>
2022-03-08 09:47:23 +0000

    Created Employee and employee test

---
+++ src/main/java/ie/gmit/Employee.java
@@ -0,0 +1,10 @@
+package ie.gmit;
+
+public class Employee {
+    private String tittle;
+
+    public void tittle (String tit){
+        this.tittle =tit;
+    }
+
+}
---
+++ src/test/java/ie/gmit/EmployeeTest.java
@@ -0,0 +1,9 @@
+/*
+NAME:Ashar Zafri
+Date:08/03/2022
+Contact details: G00366561@gmit.ie
+ */
+package ie.gmit;
+
+public class EmployeeTest {
+}
